2b55c7af5723f71f6c6ae2320c0ed8206867bcae
HuuDuc <duc11102000@gmail.com>
2022-10-28 13:36:24 +0700

    Minor fixes

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <h1>{{ head }}</h1>
+    <Button @toggle-add-task="$emit('toggle-add-task')" text="Add task" color="black" v-show="homePage"></Button>
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "header",
+  components: {
+    Button,
+},
+  props: {
+    head: {
+      type: String,
+      default: "Hello Vue !!!",
+    },
+  },
+
+  computed: {
+    homePage() {
+      if(this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>Version 1.0 :D</h3>
+        <router-link to="/">Go back</router-link>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        display: flex;
+        align-items: center;
+        column-gap: 20px;
+    }
+</style>
